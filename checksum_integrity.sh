5c5052f00690e4c2965407e23b20b521d95af0b7
Isra Boughanmi <149334111+Nayah18@users.noreply.github.com>
2024-03-13 22:28:49 +0000

    Create checksum_integrity.sh
    This Bash script is made for use on HPC systems (e.g. University of Leicester's Alice HPC) managed with Slurm. It generates MD5 checksums for a large collection of FASTQ files, ensuring data integrity for downloaded fastq files. It is aimed at files prefixed with 'SRR' and ending in '.fastq', as we previously downloaded sequence read archive (SRA) data which was then converted to fastq files. The script automates the removal of any pre-existing checksum list to prevent data duplication, ensuring a fresh start for each run. The script includes email notifications set up for job start, end, and failure states to avoid the need of manual checks."

---
+++ checksum_integrity.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=checksum_fastq
+#SBATCH --output=checksum_fastq_%j.out
+#SBATCH --error=checksum_fastq_%j.err
+#SBATCH --time=3-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem=8G
+#SBATCH --partition=medium
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=ib156@student.le.ac.uk
+
+# Check if the fastq_checksums.md5 file already exists and remove it to avoid appending to an old file
+if [ -f fastq_checksums.md5 ]; then
+    rm fastq_checksums.md5
+fi
+
+# Generate checksums for all FASTQ files starting with SRR and ending with .fastq
+for file in SRR*.fastq; do
+    md5sum "$file" >> fastq_checksums.md5
+done
